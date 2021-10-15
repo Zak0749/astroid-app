6a519a297b7e42c11cc73373290681e046ca4bfb
Zak <89150707+Zak0749@users.noreply.github.com>
2021-10-15 11:13:10 +0100

    Astroid App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface Apod {
+  copyright?: string;
+  date: Date;
+  explanation: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+  hdurl?: string;
+}
+
+const key = 'ZJwrzAUjoqgPhOYDalQbvJ2epanow6vmvYX4yAOg';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  constructor(private http: HttpClient) {}
+
+  range = new FormGroup({
+    start: new FormControl(new Date()),
+    end: new FormControl(new Date()),
+  });
+
+  reload() {
+    this.http
+      .get<Apod[]>(
+        `https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${this.range.value?.start?.toLocaleDateString(
+          'en-CA'
+        )}&end_date=${this.range.value?.end?.toLocaleDateString('en-CA')}`
+      )
+      .subscribe((data) => {
+        this.apods = data;
+      });
+  }
+
+  ngOnInit() {
+    this.reload();
+  }
+  apods: Apod[] = [];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    MatNativeDateModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
